9b21e0cc1515bf34849db36db3ea2eaddedf3d96
Malik Mazhar Ali <malik@tbwazeenah.local>
2017-09-19 14:57:20 +0400

    Socket IO added to Client and Server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const socketio = require('socket.io');
+const socketIO = require('socket.io');
 const path = require('path');
 const http = require('http');
 
@@ -10,10 +10,19 @@ var publicpath = path.join(__dirname, '../public');
 console.log(publicpath);
 
 var port = process.env.PORT || 3000;
-
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicpath));
 
-var server = http.createServer(app);
+io.on('connection', (socket2) => {      // (socket) can be called anything ... same socket will be used to check the disconnection of a user as its saved in socket.
+  
+  console.log(`New user Connected`);
+  socket2.on('disconnect', () => {
+    console.log(`User disconnected at ${new Date()}`)
+  })
+});
+
+
 server.listen(port,() => {
   console.log(`Server is listening on ${port}`)
 });
