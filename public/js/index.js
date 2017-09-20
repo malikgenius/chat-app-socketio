89235ef0aa6384b6b7534480de5f777931a5d8b3
Malik Mazhar Ali <malik@tbwazeenah.local>
2017-09-20 12:12:35 +0400

    io.emit to broadcast messages to all users

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,28 @@ socket.on('connect', function () {
   console.log('Connected to Server');
 
 });
+
+socket.emit('createMessage', {
+  from: 'client1',
+  text: 'this is my message please destribute it to others!'
+});
+
+socket.on('newMessage', function (message){
+  //console.log(`Message from server which was sent from client1 ${message}`)
+  console.log(message);
+});
+
+
+
 socket.on('disconnect', function () {
   console.log(`disconnected from server ${new Date()}`)
-})
+});
+
+// socket.on('newEmail', function (email){   // email is the first property sent from server to client .. we need to use it.
+//   console.log('New Email', email)
+// });
+//
+// socket.emit('composeEmail', {
+//   from:'client@example.com',
+//   text:'hay its client !!!'
+// });
--- server/server.js
+++ server/server.js
@@ -1,25 +1,45 @@
 const express = require('express');
 const socketIO = require('socket.io');
-const path = require('path');
-const http = require('http');
-
-var app = express();
-
+const path = require('path');     // path library let you use routes outside your folder easily .. good one to use.
+const http = require('http');     // http is must with socketio ... better to use this way in all projects.
 
 var publicpath = path.join(__dirname, '../public');
 console.log(publicpath);
 
 var port = process.env.PORT || 3000;
+var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 app.use(express.static(publicpath));
 
-io.on('connection', (socket2) => {      // (socket) can be called anything ... same socket will be used to check the disconnection of a user as its saved in socket.
-  
+io.on('connection', (socket) => {      // (socket) can be called anything ... same socket will be used to check the disconnection of a user as its saved in socket.
   console.log(`New user Connected`);
-  socket2.on('disconnect', () => {
+
+  // socket.emit('newEmail', {           // emit is used to push data to client or from client server ..
+  //   from: "malik@example.com",
+  //   text: 'Hi its your Server sending you email !!! '
+  // });
+  //
+  // socket.on('composeEmail', (email) => {      // socket.on will wait and watch for pushed data from client ..
+  //   console.log(email);
+  // })
+
+  socket.on('createMessage', (message) => {
+    var message;
+    //console.log(`message from client to distribute ${message}`);
+    console.log(message)
+    io.emit('newMessage', {     // io.emit will broadcast message to all connected users ....
+      from: message.from,     // message.from will give us clear output on console and browser when we use it later.
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
+
+  socket.on('disconnect', () => {
     console.log(`User disconnected at ${new Date()}`)
-  })
+  });
+
 });
 
 
